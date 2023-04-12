f54b93c14de98bb7affecd1d4948b028b58997b8
qwertyforce <qwertyforce@protonmail.com>
2023-04-12 15:45:16 +0300

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 import os
 from datasets import load_dataset
 import pymongo
+import random
 # from tqdm import tqdm
 
 username = os.environ["MONGO_DB_USERNAME"] 
@@ -15,30 +16,34 @@ def clean_text(text):
 myclient = pymongo.MongoClient(f"mongodb://{username}:{password}@135.181.98.162:33333/?authMechanism=DEFAULT")
 mydb = myclient["habr_dataset"]
 
-def get_clean_send_data(split):
+def send_data_split(split_name, data):
     collections = mydb.list_collection_names()
-    if split not in collections:
-        col = mydb[split]
+    if split_name not in collections:
+        col = mydb[split_name]
         col.create_index("id", unique=True)
     else:
-        col = mydb[split]
-    
-    dataset = load_dataset('IlyaGusev/habr', split=split, streaming=True)
-
-    thrash_timestamp = 1420059600             # 1577836800 01/01/2020
-    data = []
-    for idx, item in enumerate(dataset):
-        if idx % 10000==0:
-            print(idx)
-        if item["language"] == "ru" and item["time_published"] >= thrash_timestamp and item["type"] == "article":
-            new_item = {"id":item["id"], "title":item["title"],
-                        "text_markdown":item["text_markdown"], "statistics":item["statistics"],
-                        "hubs":item["hubs"], "flows":item["flows"], "tags":item["tags"], "time_published": item["time_published"]}
-            new_item["text_markdown"] = clean_text(new_item["text_markdown"])
-            data.append(new_item)
+        col = mydb[split_name]
     try:
         col.insert_many(data,ordered=False)
     except:
-        pass
+        pass 
 
-get_clean_send_data("train")
+    
+dataset = load_dataset('IlyaGusev/habr', split="train", streaming=True)
+
+thrash_timestamp =  1577836800    #  01/01/2020    
+data = []
+for idx, item in enumerate(dataset):
+    if idx % 10000==0:
+        print(idx)
+    if item["language"] == "ru" and item["time_published"] >= thrash_timestamp and item["type"] == "article":
+        new_item = {"id":item["id"], "title":item["title"],
+                    "text_markdown":item["text_markdown"], "statistics":item["statistics"],
+                    "hubs":item["hubs"], "flows":item["flows"], "tags":item["tags"], "time_published": item["time_published"]}
+        new_item["text_markdown"] = clean_text(new_item["text_markdown"])
+        data.append(new_item)
+random.shuffle(data)
+split_idx = int(len(data)*0.2)
+send_data_split("test",data[:split_idx])
+send_data_split("train",data[split_idx:])
+
